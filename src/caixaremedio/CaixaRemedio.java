c676a1196ea2036ed7086fd418ea0cfd2df8a9cd
Quejao <leotrone@hotmail.com>
2018-06-19 20:50:05 -0300

    Adicionados métodos para inserir alarmes acionados e desligados

--- src/caixaremedio/CaixaRemedio.java
+++ src/caixaremedio/CaixaRemedio.java
@@ -13,6 +13,7 @@ import DAOs.DB_Direct;
 import Model.Alarmes;
 import Model.Desligado;
 import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -27,57 +28,102 @@ public class CaixaRemedio {
     public static void main(String[] args) {
         DB_Direct db = new DB_Direct("src/DAOs/local.txt");
         printAlarmes();
+        DAOAlarmes al = new DAOAlarmes();
+        DAOAcionado ac = new DAOAcionado();
+
+//        List<Alarmes> als = al.list();
+//        alarmeAcionado(als.get(1));
+        printAcionado();
         
+//        List<Acionado> acs = ac.list();
+//        alarmeDesligado(acs.get(0));
+        printDesligado();
+
     }
-    
+
     public static void inserirAlarme(int dia, int mes, int ano, String hora) {
         DAOAlarmes al = new DAOAlarmes();
         int aux = 0;
-        
+
         List<Alarmes> als = al.list();
-        for(Alarmes a : als){
-            if(a.getIdAlarmes() > aux){
+        for (Alarmes a : als) {
+            if (a.getIdAlarmes() > aux) {
                 aux = a.getIdAlarmes();
             }
         }
-        
-        Alarmes alN = new Alarmes(aux+1);
+
+        Alarmes alN = new Alarmes(aux + 1);
         Date data = new Date(1);
         data.setDate(dia);
         data.setMonth(mes - 1);
         data.setYear(ano - 1900);
-        
+
         alN.setDataAlarmes(data);
         alN.setHoraAlarmes(hora);
         al.inserir(alN);
     }
+
+    public static void alarmeAcionado(Alarmes a) {
+        DAOAcionado dac = new DAOAcionado();
+
+        Date data = new Date(System.currentTimeMillis());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(data);
+        int hours = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        String hora = "" + hours + ":" + minute;
+
+        Acionado ac = new Acionado(a.getIdAlarmes());
+        ac.setAlarmesidAlarmes(a);
+        ac.setDataAcionado(data);
+        ac.setHoraAcionado(hora);
+        ac.setDesligadoAcionado(Boolean.FALSE);
+        dac.inserir(ac);
+
+    }
+
+    public static void alarmeDesligado(Acionado ac){
+        DAODesligado dd = new DAODesligado();
+        
+        Date data = new Date(System.currentTimeMillis());
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(data);
+        int hours = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        String hora = "" + hours + ":" + minute;
+
+        Desligado d = new Desligado(ac.getIdAcionado());
+        d.setAcionadoidAcionado(ac);
+        d.setDataDesligado(data);
+        d.setHoraDesligado(hora);
+        dd.inserir(d);
+    }
     
-    public static void printAlarmes(){
+    public static void printAlarmes() {
         DAOAlarmes al = new DAOAlarmes();
-        
+
         List<Alarmes> als = al.list();
-        for(Alarmes a : als){
+        for (Alarmes a : als) {
             System.out.println(a);
         }
     }
-    
-    public static void printAcionado(){
+
+    public static void printAcionado() {
         DAOAcionado ac = new DAOAcionado();
-       
+
         List<Acionado> acs = ac.list();
-        for(Acionado a : acs){
+        for (Acionado a : acs) {
             System.out.println(a);
         }
     }
-    
-    public static void printDesligado(){
+
+    public static void printDesligado() {
         DAODesligado dc = new DAODesligado();
-        
+
         List<Desligado> ds = dc.list();
-        for(Desligado d : ds){
+        for (Desligado d : ds) {
             System.out.println(d);
         }
     }
-    
-}
 
+}
